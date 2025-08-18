87a44dd202102f7ee344a3af0e1fef312352544a
MaxVecher <@MaxVecher>
2025-08-18 11:09:53 +0300

    KL-1-2

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,16 @@
+package lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val notificationOfAnOrderMade: String = ("Thanks for the purchase")
+
+    println(numberOfOrders)
+    println(notificationOfAnOrderMade)
+
+    var employeesOfTheOnlineStore: Int = 2000
+    //      println(employeesOfTheOnlineStore)
+
+    employeesOfTheOnlineStore = 1999
+    println(employeesOfTheOnlineStore)
+
+}
