d114f8b8b590fc399e87fbd2de5f4101d0a1994c
MaxVecher <@MaxVecher>
2025-08-18 11:59:08 +0300

    KL-1-3

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,11 @@
+package lesson_1
+fun main() {
+    val yearOfFlight = 1961
+    println(yearOfFlight)
+
+    val takeoffTime: String = ("09:07")
+    println(takeoffTime)
+
+    val boardingTime: String = ("10:55")
+    print(boardingTime)
+}
