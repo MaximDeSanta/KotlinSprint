39ac8ff57c7d22ab07a029246f10236a8bb36645
MaxVecher <@MaxVecher>
2025-08-18 12:35:48 +0300

    KL-1-4

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,22 @@
+package lesson_1
+fun main() {
+
+    val length: Long = 40868600000
+    println(length)
+
+    val age: Byte = 27
+    println(age)
+
+    val partOfDay: Double  = 0.075
+    println(partOfDay)
+
+    val seconds: Short = 6480
+    println(seconds)
+
+    val partOfYear: String = ("2.0547945205479453E-4")
+    println(partOfYear)
+
+    val apogee: Int = 327000
+    print(apogee)
+
+}
