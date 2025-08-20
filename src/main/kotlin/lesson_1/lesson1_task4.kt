158469c14b1f578e1367e9ed04195630bec61a6f
MaxVecher <@MaxVecher>
2025-08-20 12:18:57 +0300

    KL-1-4-fixed-the-errors

--- src/main/kotlin/lesson_1/lesson1_task4.kt
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -2,21 +2,17 @@ package lesson_1
 fun main() {
 
     val length: Long = 40868600000
-    println(length)
-
     val age: Byte = 27
-    println(age)
-
-    val partOfDay: Double  = 0.075
-    println(partOfDay)
-
+    val partOfDay: Float = 0.075f
     val seconds: Short = 6480
-    println(seconds)
-
-    val partOfYear: String = ("2.0547945205479453E-4")
-    println(partOfYear)
-
+    val partOfYear: Double = 2.0547945205479453E-4
     val apogee: Int = 327000
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
     print(apogee)
 
 }
