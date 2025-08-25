e665eaad496e4434243c49e70fc7e2a52a580886
MaxVecher <@MaxVecher>
2025-08-25 18:25:50 +0300

    KS-2-1-decision

--- src/main/kotlin/lesson_2/lesson_2_task_1.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -1,7 +1,11 @@
 package lesson_2
 
 fun main() {
-
-
+    val learner1: Double = 3.0
+    val learner2: Double = 4.0
+    val learner3: Double = 3.0
+    val learner4: Double = 5.0
+    val averageScore: Double = (3.0+4.0+3.0+5.0)/4
+    println(averageScore)
 
 }
